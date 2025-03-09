17d13f3dff0f79c9685a65395ebc49c1c6d73273
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-09 23:26:00 +0600

    about page is done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsLetter from '../components/NewsLetter'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}></Title>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full md:max-w-[450px]' alt="" />
+        <div className='flex flex-col justify-center gap-6 ms:w-2/6 text-gray-600'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, laudantium quae quis iure, nisi veniam fugit at hic non deserunt fugiat necessitatibus dolorum nostrum repudiandae incidunt dolores voluptates, in consectetur animi dignissimos beatae. Eveniet minus in sed ipsum quis dolore nobis libero, nisi assumenda quas, aperiam rem quo? Omnis expedita sit laborum itaque provident dignissimos blanditiis voluptatibus hic quod facere libero iure vitae saepe amet culpa consectetur, eius molestiae magni vel, possimus labore v.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ex distinctio, repellat vero rerum cum doloribus asperiores perspiciatis nihil. Dolorem, unde libero! Porro molestias harum mollitia hic, illum sunt eligendi inventore voluptate ipsa sequi fugit i</p>
+            <b className='text-gray-800'>Our mission</b>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, facere! Consectetur nemo quibusdam blanditiis qui quos sequi, ea a enim possimus itaque, ex modi qu.</p>
+        </div>
+      </div>
+      <div className='text-xl  py-4 '>
+        <Title text1={'WHY'} text2={'CHOOSE US'}></Title>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance</b>
+            <p className='text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, ratione!</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p  className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatibus, fugit dignissimos dolor eligendi facilis dolores consequuntur officia aliquid numquam?</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service</b>
+            <p  className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel cum ex, quisquam corrupti consectetur blanditiis exercitationem dolor.</p>
+          </div>
+      </div>
+      <NewsLetter />
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign up');
+  
+
+  const onSubmitHandler = async(e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+
+
+
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>
+          {currentState}
+        </p>
+          <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+
+
+
+      {
+        currentState === 'login' ? '' : <input type="text" className='w-full px-3 border border-gray-800' placeholder='Name' required/> 
+      }
+      <input type="email" className='w-full px-3 border border-gray-800' placeholder='Email' required/>
+      <input type="password" className='w-full px-3 border border-gray-800' placeholder='Password' required/>
+
+      <div className='w-full flex justify-between text-sm mt-[-8]'>
+        <p className='cursor-pointer'>forget Password</p>
+        {
+        currentState === "login" ?
+        <p onClick={()=> setCurrentState("sign Up")} className='cursor-pointer'>Create Account</p>:
+        <p onClick={()=> setCurrentState("login")} className='cursor-pointer'>Login here</p>
+      }
+      </div>
+      <button className='bg-black text-white font-white px-8 py-2 mt-4'>{currentState === "login"? 'sign in' :"Sign Up"}</button>
+  
+    </form>
   )
 }
 
