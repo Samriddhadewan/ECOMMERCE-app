9f4e8a2c50137f5968c0bbcec740690b67e2b96b
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-18 23:19:26 +0600

    admin dashboard and login design completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/sidebar'
+import { Routes,Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import { useState } from 'react'
+import Login from './pages/Login'
 
 const App = () => {
+  const [token,setToken] = useState('')
   return (
-    <div>App</div>
+    <div className='bg-gray-50 min-h-screen'>
+      {
+        token === "" ? <Login></Login> 
+        :
+              <>
+      <Navbar></Navbar>
+      <hr />
+      <div className='flex w-full'>
+      <Sidebar></Sidebar>
+      <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+        <Routes>
+          <Route path="/add" element={<Add></Add>}  />
+          <Route path="/list" element={<List></List>}  />
+          <Route path="/orders" element={<Orders></Orders>}  />
+        </Routes>
+      </div>
+      </div>
+      </> 
+      }
+    </div>
   )
 }
 
