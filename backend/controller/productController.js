9ec905a2e25cfc56f72ef56a2b2e213137b1ea1f
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-18 12:20:47 +0600

    login and register functionality added

---
+++ backend/controller/productController.js
@@ -0,0 +1,21 @@
+// add product 
+const addProduct =async(req,res)=>{
+
+}
+
+// function for list products 
+const listProducts = async(req,res)=>{
+    res.json({message: "api working"})
+}
+
+//  function for remove product 
+const removeProduct = async(req,res)=>{
+
+}
+
+//  function for single product 
+const singleProduct = async(req,res)=>{
+
+}
+
+export {addProduct, listProducts,removeProduct,singleProduct}
