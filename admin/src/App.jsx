4d462362b4dcc3e7b7277ce99c88268d9b3a2e55
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-19 18:14:54 +0600

    add product backend api connected to the admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
       <Sidebar></Sidebar>
       <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
         <Routes>
-          <Route path="/add" element={<Add></Add>}  />
+          <Route path="/add" element={<Add token={token} ></Add>}  />
           <Route path="/list" element={<List></List>}  />
           <Route path="/orders" element={<Orders></Orders>}  />
         </Routes>
